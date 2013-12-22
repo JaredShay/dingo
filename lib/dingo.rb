9623f2ffecfdc81af8aef4194dd880a08ead020b
Jared Shay <jared.shay@gmail.com>
2013-12-23 09:08:53 +1100

    read in words from file

--- lib/dingo.rb
+++ lib/dingo.rb
@@ -1,11 +1,19 @@
 require 'enumerator'
 
 class Dingo
-  def self.words
-    Enumerator.new do |y|
-      loop do
-        y.yield "one"
+  class << self
+    def words
+      Enumerator.new do |y|
+        loop do
+          y.yield aussie_words.sample
+        end
       end
+    end
+
+    private
+
+    def aussie_words
+      @words ||= File.read(File.expand_path("../../dingo.txt", __FILE__)).lines.map(&:chomp)
     end
   end
 end
